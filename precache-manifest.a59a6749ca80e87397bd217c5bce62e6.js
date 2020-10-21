e06212f21d60814997807636ac594a7b8f5aca1c
Yitzhak Mizrahi <yitzhak@firmusai.com>
2020-10-21 17:04:03 +0300

    Updates

---
+++ precache-manifest.a59a6749ca80e87397bd217c5bce62e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3f98465db990d46c7401938fce1cd89",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0e5cc5ec4908abe2f08b",
+    "url": "/monsters-rolodex/static/css/main.0324825b.chunk.css"
+  },
+  {
+    "revision": "f446d9444977f330bfbe",
+    "url": "/monsters-rolodex/static/js/2.70969f69.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/monsters-rolodex/static/js/2.70969f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e5cc5ec4908abe2f08b",
+    "url": "/monsters-rolodex/static/js/main.b2918d5e.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
